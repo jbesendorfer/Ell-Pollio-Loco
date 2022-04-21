448d1df6175149c0bf1b74d00b638d28f4a36ece
Johannes Besendorfer <johannes.besendorfer@me.com>
2022-04-21 21:02:07 +0200

    endboss_animiert

--- models/character.class.js
+++ models/character.class.js
@@ -45,10 +45,8 @@ class Character extends MovableObject {
 
             if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 //walk animation
-                let i = this.currentImage % this.IMAGES_WALKING.length;  // let i = 7 % 6; => 1, Rest 1
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
+
             }
          }, 50);
     }
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,8 +1,10 @@
-class Endboss extends MovableObjects {
+class Endboss extends MovableObject {
 
 
     IMAGES_WALKING = [
         'img/4.Secuencias_Enemy_gigantขn-Doคa_Gallinota-/2.Ateciขn-ataque/1.Alerta/G5.png',
+
+
         'img/4.Secuencias_Enemy_gigantขn-Doคa_Gallinota-/2.Ateciขn-ataque/1.Alerta/G6.png',
         'img/4.Secuencias_Enemy_gigantขn-Doคa_Gallinota-/2.Ateciขn-ataque/1.Alerta/G7.png',
         'img/4.Secuencias_Enemy_gigantขn-Doคa_Gallinota-/2.Ateciขn-ataque/1.Alerta/G8.png',
@@ -17,5 +19,14 @@ class Endboss extends MovableObjects {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
         this.x = 700;
+        this.animate();
     }
+
+    animate(){
+
+               setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);          
+         }, 200);
+    }
+
 }
