64660bf7dd39d639edd020d0e8262c944ad1618e
Johannes Besendorfer <johannes.besendorfer@me.com>
2022-04-22 20:38:18 +0200

    Statusbar_III

---
+++ models/status-bar.class.js
@@ -0,0 +1,43 @@
+class StatusBar extends DrawableObjects {
+
+
+    IMAGES = [
+        'img/7.Marcadores/Barra/Marcador vida/azul/0_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/20_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/40_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/60_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/80_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/100_.png',
+    ];
+
+    percentage = 100;
+
+
+    constructor(){
+        this.loadImages(this.IMAGES)
+    }
+
+    // setPercentage(50);
+    setPercentage() {
+        this.percentage = this.percentage; // => 0...5
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+
+        resolveImageIndex(){
+            if(this.percentage == 100) {
+                return 5;
+            }   else if (this.percentage > 80) {
+                return 4;
+            }   else if (this.percentage > 60) {
+                return 3;    
+            }   else if (this.percentage > 40) {
+                return 2;    
+            }   else if (this.percentage > 20) {
+                return 1;  
+            }   else  {
+                return 0;  
+        }   
+    }
+}
