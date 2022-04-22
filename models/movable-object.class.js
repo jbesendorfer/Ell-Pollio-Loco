69b52c4e9d5b438e50c0dcc03d03b82ac470eea9
Johannes Besendorfer <johannes.besendorfer@me.com>
2022-04-22 09:46:49 +0200

    Refactoring

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -33,6 +33,18 @@ class MovableObject {
         this.img.src = path;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height); 
+    }
+
+    drawFrame(ctx){
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
     /**
      * 
      * @param {Array} arr - ['img/image1.png', img/image2.png', ...]
