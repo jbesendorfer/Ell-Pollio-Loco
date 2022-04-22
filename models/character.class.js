2862e939fb9ccdca1879c2efb600ecc462cb009a
Johannes Besendorfer <johannes.besendorfer@me.com>
2022-04-22 07:53:41 +0200

    Jump_animation

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,20 @@ class Character extends MovableObject {
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-25.png',
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-26.png'
     ];
+
+    IMAGES_JUMPING = [
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-31.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-32.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-33.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-34.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-35.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-36.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-37.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-38.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-39.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-40.png'
+    ];
+
     world;
     walking_sound = new Audio('audio/running.mp3');
 
@@ -19,6 +33,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
@@ -43,11 +58,15 @@ class Character extends MovableObject {
 
         setInterval(() => {
 
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                //walk animation
-                this.playAnimation(this.IMAGES_WALKING);
+            if(this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
 
-            }
+                if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    //walk animation
+                    this.playAnimation(this.IMAGES_WALKING);
+                    }
+                }    
          }, 50);
     }
   
